7efbdf1297923c4899bcd7058ab0bb5543153a75
Xact1fy <maxispog2022@gmail.com>
2025-10-25 00:07:35 +0530

    Update hiddenF.py

--- hiddenF.py
+++ hiddenF.py
@@ -3,5 +3,6 @@ import os
 def hidden_function():
     os.system('dir < hidden.txt')
 
-    with open("hidden.txt", "r") as file:
+    with open("hidden.txt", "w") as file:
+
         hidden_code = file.write("you found the hidden code! 😁😁😁")
